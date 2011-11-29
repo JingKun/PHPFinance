fae78bc5634613a1fdb3ece613491bb2a6704936
Michael Cordingley <Michael.Cordingley@gmail.com>
2011-11-29 10:36:11 -0500

    Cleaned up the stub, removed unnecessary closing tag from the end.

--- stub.php
+++ stub.php
@@ -1,9 +1,7 @@
 <?php
 Phar::mapPhar('PHPFinance.phar');
-
 spl_autoload_register(function ($className) {
 	$classPath = 'phar://PHPFinance.phar/lib/'.substr(str_replace('\\', '/', $className).'.php', 11);
-
 	@include($classPath);
 });
-__HALT_COMPILER(); ?>
+__HALT_COMPILER();
